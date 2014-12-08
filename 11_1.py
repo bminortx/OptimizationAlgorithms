3d5d43cc99fccf573c8ba69273edcfc7b8bbd49d
Brandon Minor <bminortx@gwmail.gwu.edu>
2014-12-07 20:30:19 -0700

    Completed 11_1

--- 11_1.py
+++ 11_1.py
@@ -4,6 +4,19 @@
 # function f_0
 
 import matplotlib.pyplot as plt
-import scipy
 import numpy as np
 
+
+def phi(x):
+    return - np.log(x - 2) - np.log(4 - x)
+
+def f(x):
+    return pow(x, 2) + 1
+
+# MAIN FUNCTION
+if __name__ == '__main__':
+    plt.figure(1)
+    x = np.arange(2.01, 3.99, 0.02)
+    for t in np.arange(0, 10, .1):
+        plt.plot(x, f(x), 'bo', x, f(x)+phi(x)/t, 'k')
+    plt.show()
